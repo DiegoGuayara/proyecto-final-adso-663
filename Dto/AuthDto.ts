7800300746fdf374de4d94bea59a375ff9c66f76
DiegoGuayara <diegofguayara9707@gmail.com>
2025-04-07 14:23:28 -0500

    Creacion de la rama branch-mauricio

--- Dto/AuthDto.ts
+++ Dto/AuthDto.ts
@@ -1,31 +1,27 @@
 class Auth {
-    private _email: string;
-    private _password: string
-    constructor(
-        email: string,
-        password: string
-    ) {
-        this._email = email;
-        this._password = password
-    }
-    // Getters
-    get email(): string {
-        return this._email;
-    }
+  private _email: string;
+  private _password: string;
+  constructor(email: string, password: string) {
+    this._email = email;
+    this._password = password;
+  }
+  // Getters
+  get email(): string {
+    return this._email;
+  }
 
-    get password(): string {
-        return this._password;
-    }
+  get password(): string {
+    return this._password;
+  }
 
-    // Setters
-    set email(email: string) {
-        this._email = email;
-    }
+  // Setters
+  set email(email: string) {
+    this._email = email;
+  }
 
-    set password(password: string) {
-        this._password = password;
-    }
-
+  set password(password: string) {
+    this._password = password;
+  }
 }
 
 export default Auth;
--- app.ts
+++ app.ts
@@ -1,23 +1,25 @@
 import express from "express";
-import bodyParser from 'body-parser';
+import bodyParser from "body-parser";
 
-import register from './routes/register';
-import auth from './routes/auth';
-import profile from './routes/profile';
+import register from "./routes/register";
+import auth from "./routes/auth";
+import profile from "./routes/profile";
 
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express().use(bodyParser.json());
 
-app.use('/register', register);
-app.use('/auth', auth);
-app.use('/profile', profile);
+app.use("/register", register);
+app.use("/auth", auth);
+app.use("/profile", profile);
 
 const PORT = process.env.PORT || 10101;
 
-app.listen(PORT, () => {
-  console.log("Servidor ejecutándose en el puerto: ", PORT);
-}).on("error", (error) => {
-  throw new Error(error.message);
-});
+app
+  .listen(PORT, () => {
+    console.log("Servidor ejecutándose en el puerto: ", PORT);
+  })
+  .on("error", (error) => {
+    throw new Error(error.message);
+  });
