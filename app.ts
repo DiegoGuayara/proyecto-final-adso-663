f718f8c85838d03660fb85bc0801e3302024ad4b
DiegoGuayara <diegofguayara9707@gmail.com>
2025-04-07 14:25:36 -0500

    Archivos organizados

--- app.ts
+++ app.ts
@@ -1,18 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
 
-import register from "./routes/register";
-import auth from "./routes/auth";
-import profile from "./routes/profile";
-
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express().use(bodyParser.json());
-
-app.use("/register", register);
-app.use("/auth", auth);
-app.use("/profile", profile);
 
 const PORT = process.env.PORT || 10101;
 
