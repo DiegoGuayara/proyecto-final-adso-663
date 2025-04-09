ceb091408cb55276ba239eae7d5da9207dd03a10
P1 <morales.vidal@gmail.com>
2025-04-08 22:22:22 -0500

    AGREGADO PersonaDto

---
+++ Dto/PersonaDto.ts
@@ -0,0 +1,10 @@
+interface personaDto {
+    id_persona: number;
+    tipo_persona: string;
+    nombre: string;
+    tipo_documento: string;
+    num_documento: string;
+    direccion: string;
+    telefono: string;
+    email: string;
+}
