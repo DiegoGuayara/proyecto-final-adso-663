229d5173439d5cbd3697fd882348635eae63302d
Mauriciom20 <morales.vidal2001@gmail.com>
2025-04-08 23:19:06 -0500

    AGREGADO ArticuloDto

---
+++ Dto/ArticuloDto.ts
@@ -0,0 +1,11 @@
+interface personaDto {
+    id_articulo: number;
+    id_categoria:number;
+    codigo: string;
+    nombre: string;
+    precio_venta: number;
+    stock: number;
+    descripcion: string;
+    imagen: string;
+    estado: boolean;
+}
