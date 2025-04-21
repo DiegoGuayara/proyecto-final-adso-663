e0f2853b0f97ac59c7ad892c7d74d0b6202eefcb
Mauriciom20 <morales.vidal2001@gmail.com>
2025-04-21 01:08:53 -0500

    Modificado VerifyToken.ts en middleware, instalada libreria de jsonwebtoken y Agredado generateToken.ts en Helpers

--- middleware/VerifyToken.ts
+++ middleware/VerifyToken.ts
@@ -4,36 +4,27 @@ import dotenv from "dotenv";
 dotenv.config();
 
 
-interface JwtPayload {
-    data: {id: number},
-    exp: number,
-    iat: number
-}
-
-
 const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
-    let authorization = req.get('Authorization');    
-    if (authorization) {
-        const token = authorization.split(' ')[1]        
-        if (!token) {
-            return res.status(401).json(
-                { status: 'you have not sent a token' }
-            );
-        };
-        try {
-            let decoded = jwt.verify(token, process.env.KEY_TOKEN as string) as JwtPayload;            
-            req.body.id = decoded.data.id;
-            next()
-        } catch (error) {
-            return res.status(403).json(
-                { status: 'Unauthorized' }
-            );
-        }
+    const authorization = req.get("Authorization");
+  
+    if (!authorization || !authorization.startsWith("Bearer ")) {
+      return res.status(403).json({ status: "Authorization header is required" });
     }
-    return res.status(403).json(
-        { status: "The Authorization header is required"}
-    );
-}
-
-
-export default verifyToken;
+  
+    const token = authorization.split(" ")[1];
+  
+    if (!token) {
+      return res.status(401).json({ status: "Token not provided" });
+    }
+  
+    try {
+      const decoded: any = jwt.verify(token, process.env.KEY_TOKEN as string);
+      req.body.id = decoded.data.id;
+      next();
+    } catch (error) {
+      return res.status(403).json({ status: "Unauthorized" });
+    }
+  };
+  
+  export default verifyToken;
+  
